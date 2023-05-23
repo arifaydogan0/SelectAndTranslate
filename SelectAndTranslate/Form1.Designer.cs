406a37a82096e7d7b9b75e6843a87521239be08a
ARİF AYDOĞAN <aydoganarif@yahoo.com>
2023-05-23 03:02:44 +0300

    Add project files.

---
+++ SelectAndTranslate/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SelectAndTranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(135, 75);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(585, 245);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 555;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(567, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(18, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "x";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::SelectAndTranslate.Resource1.copy;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(567, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(18, 20);
+            this.button2.TabIndex = 2;
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(524, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Copied";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(585, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "\"";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SelectAndTranslate/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Web;
+using System.Threading.Tasks;
+using Gma.System.MouseKeyHook;
+using System.Collections.Generic;
+
+namespace SelectAndTranslate
+{
+    public partial class Form1 : Form
+    {
+        string _sourceText = "";
+        public string SourceText
+        {
+            get => _sourceText;
+            set
+            {
+                if (_sourceText == value)
+                    return;
+                _sourceText = value;
+
+                richTextBox1.Clear();
+                richTextBox1.AppendText(Translate(_sourceText, "auto", TargetLang).Replace("\\n","\n").Replace("\\r","\r"));
+
+            }
+        }
+        public string TargetLang { get; set; } = "tr";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            globalMouseHook = Hook.GlobalEvents(); // Not: uygulama kancası için bunun yerine Hook.AppEvents() öğesini kullanın.
+
+            globalMouseHook.MouseDragFinished += MouseDragFinished;   // DragFinished olayını bir işlevle bağlayın. Çift tıklama ile aynı, bu yüzden buraya yazmadım.
+            //globalMouseHook.MouseDoubleClick += MouseDoubleClicked;   // MouseDoubleClick olayını MouseDoubleClicked adlı bir işlevle bağlayın. 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TODO: buraya pencerenin boyutunu ve konumunu ayarlayan kodları yaz.
+            if (Test())
+                this.Text = "Bağlantı var";
+            else
+                this.Text = "Bağlantı yok";
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //her cycle da seçili metin var mı diye baksın, varsa tüm metni çevirip textboxa yazsın
+
+        }
+
+
+
+        /// <summary>
+        /// İnternet bağlantısı var mı diye test ediliyor.
+        /// </summary>
+        /// <returns></returns>
+        public bool Test()
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create("http://google.com");
+                WebResponse response = request.GetResponse();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Bu metot Google Translate API kullanılır. Parametre olarak bir kaynak metin, kaynak dil(veya auto) ve hedef dil verilerek translate edilir ve sonucu string olarak döndürür.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public static String Translate(String input, string from, string to)
+        {
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={from}&tl={to}&dt=t&q={HttpUtility.UrlEncode(input)}";
+            var result = new WebClient { Encoding = System.Text.Encoding.UTF8 }.DownloadString(url);
+            try
+            {
+                //result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+
+                List<int> quoteIndex = new System.Collections.Generic.List<int>();
+                for (int i = 0; i < result.Length; i++)
+                    if (result[i] == '\"')
+                        quoteIndex.Add(i);
+                List<string> contents = new List<string>();
+                for (int i = 0; i < quoteIndex.Count - 1; i = i + 2)
+                {
+                    string content = result.Substring(quoteIndex[i] + 1, quoteIndex[i + 1] - quoteIndex[i] - 1);
+                    contents.Add(content);
+                }
+                string filteredResult = "";
+                for (int i = 0; i < contents.Count; i++)
+                {
+                    if (input.Contains(contents[i].Replace("\\n","").Replace("\\r","")) && contents[i].Length >= 7)
+                        filteredResult += contents[i-1];
+                }
+                return filteredResult;
+            }
+            catch
+            {
+                return "error";
+            }
+        }
+
+
+
+        // GUI gecikmelerini önlemek için işlevi zaman uyumsuz hale getiriyorum.
+        private async void MouseDragFinished(object sender, System.Windows.Forms.MouseEventArgs e) => await GetSelectedText();
+        //private async void MouseDoubleClicked(object sender, System.Windows.Forms.MouseEventArgs e) => await GetSelectedText();
+        private IKeyboardMouseEvents globalMouseHook;
+        /// <summary>
+        /// Bu metot windows işletim sisteminde herhangi bir uygulamada seçilen metni yakalayıp döndürür.
+        /// </summary>
+        /// <returns></returns>
+        async Task GetSelectedText()
+        {
+            System.Windows.IDataObject tmpClipboard = System.Windows.Clipboard.GetDataObject();  // Daha sonra geri yüklemek için panonun mevcut içeriğini kaydedin.
+            System.Windows.Clipboard.Clear();
+            await Task.Delay(50);   // Küçük bir gecikmenin daha güvenli olacağını düşünüyorum.Kaldırabilirsiniz ama dikkatli olun.
+
+            System.Windows.Forms.SendKeys.SendWait("^c");   // Send Ctrl+C, which is "copy"
+            await Task.Delay(50);
+
+            if (System.Windows.Clipboard.ContainsText())
+            {
+                SourceText = System.Windows.Clipboard.GetText();
+            }
+
+            System.Windows.Clipboard.SetDataObject(tmpClipboard);  // Restore the Clipboard.
+        }
+    }
+}
